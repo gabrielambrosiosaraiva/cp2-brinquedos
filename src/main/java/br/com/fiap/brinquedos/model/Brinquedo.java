d1a2c814e552fa893697e4d494ec3370c9c8cb51
Gabriel <saraiva.gabrielambrosio@gmail.com>
2026-04-29 20:39:31 -0300

    adicionando brinquedo.java

--- src/main/java/br/com/fiap/brinquedos/model/Brinquedo.java
+++ src/main/java/br/com/fiap/brinquedos/model/Brinquedo.java
@@ -1,4 +1,19 @@
 package br.com.fiap.brinquedos.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TDS_TB_BRINQUEDOS")
 public class Brinquedo {
+
+    @Id
+    private Long id;
+
+    private String nome;
+    private String tipo;
+    private Integer classificacao;
+    private String tamanho;
+    private Double preco;
+
+
 }
